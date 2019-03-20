1f0114e4971a207b9c7454d176c9bb0f6d8f9274
BryanPinto <bryaan.pintoo@gmail.com>
2019-03-20 19:13:20 -0300

    Vista y controlador de ListarCasos

---
+++ OrdenDeTrabajo/Controllers/ListarCasosController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebSolicitudes.Controllers
+{
+    public class ListarCasosController : Controller
+    {
+        //
+        // GET: /ListarCasos/
+
+        public ActionResult ListarCasos()
+        {
+            return View();
+        }
+
+    }
+}
